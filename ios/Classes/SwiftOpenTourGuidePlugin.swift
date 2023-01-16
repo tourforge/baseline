7f29b7a6dd672c2ae7f7a902aa24f8eb24f1cc18
sl4b <57822954+sl4b@users.noreply.github.com>
2023-01-15 21:14:49 -0500

    Make example work on iOS

--- ios/Classes/SwiftOpenTourGuidePlugin.swift
+++ ios/Classes/SwiftOpenTourGuidePlugin.swift
@@ -3,12 +3,9 @@ import UIKit
 
 public class SwiftOpenTourGuidePlugin: NSObject, FlutterPlugin {
   public static func register(with registrar: FlutterPluginRegistrar) {
-    let channel = FlutterMethodChannel(name: "opentourguide", binaryMessenger: registrar.messenger())
-    let instance = SwiftOpenTourGuidePlugin()
-    registrar.addMethodCallDelegate(instance, channel: channel)
-  }
-
-  public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
-    result("iOS " + UIDevice.current.systemVersion)
+      let factory = MapLibreNativeViewFactory(messenger: registrar.messenger())
+      registrar.register(
+          factory,
+          withId: "org.opentourbuilder.guide.MapLibrePlatformView")
   }
 }
