39d0cfb889c6d97409411bf2823461304a54473f
invpt <57822954+invpt@users.noreply.github.com>
2024-03-21 20:37:48 -0400

    Fix iOS build

--- ios/Classes/TourForgeBaselinePlugin.m
+++ ios/Classes/TourForgeBaselinePlugin.m
@@ -1,11 +1,11 @@
 #import "TourForgeBaselinePlugin.h"
-#if __has_include(<tourforge/tourforge-Swift.h>)
-#import <tourforge/tourforge-Swift.h>
+#if __has_include(<tourforge_baseline/tourforge_baseline-Swift.h>)
+#import <tourforge_baseline/tourforge_baseline-Swift.h>
 #else
 // Support project import fallback if the generated compatibility header
 // is not copied when this plugin is created as a library.
 // https://forums.swift.org/t/swift-static-libraries-dont-copy-generated-objective-c-header/19816
-#import "tourforge-Swift.h"
+#import "tourforge_baseline-Swift.h"
 #endif
 
 @implementation TourForgeBaselinePlugin
